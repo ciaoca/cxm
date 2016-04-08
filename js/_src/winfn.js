6cddb3689f51f6b1fd7412e5b7a6de46222e4474
ciaoca <ciaoca@gmail.com>
2016-04-08 16:47:40 +0800

    表单验证更换为 cxValidation 二维码结构调整 兼容性优化

--- js/_src/winfn.js
+++ js/_src/winfn.js
@@ -2,7 +2,6 @@
  * 添加到全局（window）中简单的方法
  *
  * CallLoading          全屏 Loading 遮罩
- * CallFormValidation   表单验证
  * CallFormAjax         表单 Ajax 提交
  * CallSms              发送短信
  * CallQrcode           展示二维码
@@ -25,6 +24,8 @@
     show: function(text) {
       if (typeof text === 'string' && text.length) {
         dom.attr('title', text);
+      } else {
+        dom.removeAttr('title');
       };
       dom.appendTo('body');
     },
@@ -38,367 +39,6 @@
 
 
 /**
- * 表单验证
- * data: 2015-12-29
- * form {jQueryObject} 要提交的表单
- * options {object}
- *   success {function} 验证成功的回调
- *   error {function} 验证失败的回调
- */
-(function() {
-  var cxValidation = {
-    dom: {}
-  };
-
-  cxValidation.init = function() {
-    var self = this;
-
-    self.dom.tip = $('<div></div>', {class: 'cxvalidation_tip'});
-
-    self.defaults = {
-      success: undefined,
-      error: undefined
-    };
-
-    self.result = {
-      status: true
-    };
-
-    // 验证方法
-    self.validFun = {
-      _required: function(el) {
-        if (el.type === 'checkbox' || el.type === 'radio') {
-          return el.checked ? true : false;
-        } else {
-          return el.value.length ? true : false;
-        };
-      },
-      _equals: function(el, id) {
-        return el.value == document.getElementById(id).value;
-      },
-      _minSize: function(el, size) {
-        return el.value.length >= size;
-      },
-      _maxSize: function(el, size) {
-        return el.value.length <= size;
-      },
-      _min: function(el, size) {
-        return parseFloat(el.value) >= size;
-      },
-      _max: function(el, size) {
-        return parseFloat(el.value) <= size;
-      },
-      _integer: function(el, id) {
-        return /^[\-\+]?\d+$/.test(el.value);
-      },
-      _number: function(el) {
-        return /^[\-\+]?((([0-9]{1,3})([,][0-9]{3})*)|([0-9]+))?([\.]([0-9]+))?$/.test(el.value);
-      },
-      _onlyNumber: function(el) {
-        return /^[0-9]+$/.test(el.value);
-      },
-      _onlyNumberSp: function(el) {
-        return /^[0-9\ ]+$/.test(el.value);
-      },
-      _onlyLetter: function(el) {
-        return /^[a-zA-Z]+$/.test(el.value);
-      },
-      _onlyLetterSp: function(el) {
-        return /^[a-zA-Z\ ]+$/.test(el.value);
-      },
-      _onlyLetterNumber: function(el) {
-        return /^[0-9a-zA-Z]+$/.test(el.value);
-      },
-      _onlyLetterNumberSp: function(el) {
-        return /^[0-9a-zA-Z\ ]+$/.test(el.value);
-      },
-      _email: function(el, id) {
-        return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(el.value);
-      },
-      _phone: function(el, id) {
-        return /^([\+][0-9]{1,3}[ \.\-])?([\(]{1}[0-9]{2,6}[\)])?([0-9 \.\-\/]{3,20})((x|ext|extension)[ ]?[0-9]{1,4})?$/.test(el.value);
-      },
-      _url: function(el, id) {
-        return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(el.value);
-      },
-      _chinese: function(el, id) {
-        return /^[\u4E00-\u9FA5]+$/.test(el.value);
-      },
-      _chinaId: function(el, id) {
-        return /^[1-9]\d{5}[1-9]\d{3}(((0[13578]|1[02])(0[1-9]|[12]\d|3[0-1]))|((0[469]|11)(0[1-9]|[12]\d|30))|(02(0[1-9]|[12]\d)))(\d{4}|\d{3}[xX])$/.test(el.value);
-      },
-      _chinaIdLoose: function(el, id) {
-        return /^(\d{18}|\d{15}|\d{17}[xX])$/.test(el.value);
-      },
-      _chinaZip: function(el, id) {
-        return /^\d{6}$/.test(el.value);
-      },
-      _qq: function(el, id) {
-        return /^[1-9]\d{4,10}$/.test(el.value);
-      },
-      _call: function(el, functionName) {
-        var _namespaces;
-        var _scope;
-        var _fun;
-        var _val;
-
-        if (functionName.indexOf('.') >= 0) {
-          _namespaces = functionName.split('.');
-          _scope = window;
-
-          while (_namespaces.length) {
-            _scope = _scope[_namespaces.shift()];
-          };
-          _fun = _scope;
-        } else {
-          _fun = window[functionName]
-        };
-
-        return typeof _fun === 'function' ? _fun(el.value) : true;
-      }
-    };
-  };
-
-  cxValidation.getMessage = function(str, isTrue) {
-    return isTrue ? '' : str;
-  };
-
-  cxValidation.toFocus = function(el) {
-    if ($(el).is(':visible')) {
-      el.focus();
-    };
-  };
-
-  cxValidation.validItem = function(el) {
-    var self = this;
-    var _ruleStr = el.dataset.validation;
-    var _ruleArr = [];
-    var _result = {
-      status: true
-    };
-    var _ruleOpt;
-
-    if (typeof _ruleStr === 'string' && _ruleStr.length) {
-      _ruleArr = _ruleStr.replace(/\s|\[[^\]]*\]/g, '').split(',');
-    };
-
-    for (var i = 0, l = _ruleArr.length; i < l; i++) {
-
-      switch(_ruleArr[i]) {
-        case 'required':
-          _result.status = self.validFun._required(el);
-          break
-        case 'equals':
-          _ruleOpt = _ruleStr.match(/equals\[([^\]]+)\]/)[1];
-          _result.status = self.validFun._equals(el, _ruleOpt);
-          _result.message = self.getMessage('两次输入的不一致', _result.status);
-          break
-        case 'minSize':
-          _ruleOpt = _ruleStr.match(/minSize\[([^\]]+)\]/)[1];
-          _result.status = el.value.length ? self.validFun._minSize(el, _ruleOpt) : true;
-          _result.message = self.getMessage('最少 ' + _ruleOpt + ' 个字符', _result.status);
-          break
-        case 'maxSize':
-          _ruleOpt = _ruleStr.match(/maxSize\[([^\]]+)\]/)[1];
-          _result.status = el.value.length ? self.validFun._maxSize(el, _ruleOpt) : true;
-          _result.message = self.getMessage('最多 ' + _ruleOpt + ' 个字符', _result.status);
-          break
-        case 'min':
-          _ruleOpt = _ruleStr.match(/min\[([^\]]+)\]/)[1];
-          _result.status = el.value.length ? self.validFun._min(el, _ruleOpt) : true;
-          _result.message = self.getMessage('最小值为 ' + _ruleOpt, _result.status);
-          break
-        case 'max':
-          _ruleOpt = _ruleStr.match(/max\[([^\]]+)\]/)[1];
-          _result.status = el.value.length ? self.validFun._max(el, _ruleOpt) : true;
-          _result.message = self.getMessage('最大值为 ' + _ruleOpt, _result.status);
-          break
-        case 'integer':
-          _result.status = el.value.length ? self.validFun._integer(el) : true;
-          _result.message = self.getMessage('无效的整数', _result.status);
-          break
-        case 'number':
-          _result.status = el.value.length ? self.validFun._number(el) : true;
-          _result.message = self.getMessage('无效的数值', _result.status);
-          break
-        case 'onlyNumber':
-          _result.status = el.value.length ? self.validFun._onlyNumber(el) : true;
-          _result.message = self.getMessage('只能填写数字', _result.status);
-          break
-        case 'onlyNumberSp':
-          _result.status = el.value.length ? self.validFun._onlyNumberSp(el) : true;
-          _result.message = self.getMessage('只能填写数字和空格', _result.status);
-          break
-        case 'onlyLetter':
-          _result.status = el.value.length ? self.validFun._onlyLetter(el) : true;
-          _result.message = self.getMessage('只能填写英文字母', _result.status);
-          break
-        case 'onlyLetterSp':
-          _result.status = el.value.length ? self.validFun._onlyLetterSp(el) : true;
-          _result.message = self.getMessage('只能填写英文字母和空格', _result.status);
-          break
-        case 'onlyLetterNumber':
-          _result.status = el.value.length ? self.validFun._onlyLetterNumber(el) : true;
-          _result.message = self.getMessage('只能填写英文字母与数字', _result.status);
-          break
-        case 'onlyLetterNumberSp':
-          _result.status = el.value.length ? self.validFun._onlyLetterNumberSp(el) : true;
-          _result.message = self.getMessage('只能填写英文字母、数字、空格', _result.status);
-          break
-        case 'email':
-          _result.status = el.value.length ? self.validFun._email(el) : true;
-          _result.message = self.getMessage('无效的邮件地址', _result.status);
-          break
-        case 'phone':
-          _result.status = el.value.length ? self.validFun._phone(el) : true;
-          _result.message = self.getMessage('无效的电话号码', _result.status);
-          break
-        case 'url':
-          _result.status = el.value.length ? self.validFun._url(el) : true;
-          _result.message = self.getMessage('无效的网址', _result.status);
-          break
-        case 'chinese':
-          _result.status = el.value.length ? self.validFun._chinese(el) : true;
-          _result.message = self.getMessage('只能填写中文汉字', _result.status);
-          break
-        case 'chinaId':
-          _result.status = el.value.length ? self.validFun._chinaId(el) : true;
-          _result.message = self.getMessage('无效的身份证号码', _result.status);
-          break
-        case 'chinaIdLoose':
-          _result.status = el.value.length ? self.validFun._chinaIdLoose(el) : true;
-          _result.message = self.getMessage('无效的身份证号码', _result.status);
-          break
-        case 'chinaZip':
-          _result.status = el.value.length ? self.validFun._chinaZip(el) : true;
-          _result.message = self.getMessage('无效的邮政编码', _result.status);
-          break
-        case 'qq':
-          _result.status = el.value.length ? self.validFun._qq(el) : true;
-          _result.message = self.getMessage('无效的 QQ 号码', _result.status);
-          break
-
-        // 自定义函数验证：
-        // 只有返回 false 或 returnObject.status 为 false 时，属于不通过；
-        // 返回其他值（包括 undefined、null等）均视为验证通过。
-        case 'call':
-          _ruleOpt = _ruleStr.match(/call\[([^\]]+)\]/)[1];
-          _result = self.validFun._call(el, _ruleOpt);
-          break
-
-        // not default
-      };
-
-      if (!$.isPlainObject(_result)) {
-        _result = {
-          status: _result
-        };
-      };
-
-      if (_result.status === false) {
-        _result.rule = _ruleArr[i];
-        break
-      };
-    };
-
-    return _result;
-  };
-
-  cxValidation.validForm = function(form, options, errorCallback) {
-    var self = this;
-    var result = $.extend({}, self.result);
-    var el;
-
-    if (typeof self.closeTipWait === 'undefined') {
-      clearTimeout(self.closeTipWait);
-    };
-    self.dom.tip.appendTo('body');
-
-    if (typeof options === 'function') {
-      options = {
-        success: options
-      };
-    };
-
-    options = $.extend({}, self.defaults, options);
-
-    if (typeof errorCallback === 'function') {
-      options.error = errorCallback;
-    };
-
-    var inputs = [];
-    inputs = inputs.concat(form.find('input'), form.find('textarea'), form.find('select'));
-
-    $.each(inputs, function(index, item) {
-      var _result = self.validItem(item);
-
-      if (_result === false || _result.status === false) {
-        el = item;
-        result.status = false;
-
-        if (typeof _result.message === 'string' && _result.message.length) {
-          result.message = _result.message;
-        };
-
-        if (typeof _result.rule === 'string' && _result.rule.length) {
-          result.rule = _result.rule;
-        };
-
-        return false;
-      };
-    });
-
-    if (result.status === true && typeof options.success === 'function') {
-      options.success(form);
-
-    } else if (result.status === false) {
-      if (typeof options.error === 'function') {
-        options.error(el, result);
-
-      } else if (el) {
-        if (typeof el.dataset.validationMessage === 'string' && el.dataset.validationMessage.length) {
-          try {
-            options.message = JSON.parse(el.dataset.validationMessage);
-
-            if (typeof options.message[result.rule] === 'string' && options.message[result.rule].length) {
-              result.message = options.message[result.rule];
-            };
-          } catch (e) {
-            result.message = el.dataset.validationMessage;
-          };
-        };
-
-        if (typeof result.message === 'string' && result.message.length) {
-          $.cxDialog({
-            title: '提示',
-            info: result.message,
-            ok: function() {
-              self.toFocus(el);
-            }
-          });
-
-          // 在顶部提示，输入框获取焦点时，会被顶起导致看不到提示内容，若不获取焦点又不太明白是哪个输入框的提示
-          // self.toFocus(el);
-          // self.dom.tip.html(result.message).addClass('show');
-          // self.closeTipWait = setTimeout(function() {
-            // self.dom.tip.removeClass('show');
-          // }, 3000);
-        } else {
-          self.toFocus(el);
-        };
-      };
-    };
-
-    return result.status;
-  };
-
-  cxValidation.init();
-
-  window.CallFormValidation = cxValidation.validForm.bind(cxValidation);
-})();
-
-
-/**
  * 表单 Ajax 提交
  * form {jQueryObject} 要提交的表单
  * options {object}
@@ -407,7 +47,42 @@
  *   error {function} 提交完成，状态为错误时执行的方法
  */
 (function() {
-  window.CallFormAjax = function(form, options, errorCallback) {
+  var formAjax = function() {
+    return formAjax.submit.apply(formAjax, arguments);
+  };
+
+  formAjax.finish = function(data) {
+    if (typeof data.nextUrl === 'string' && data.nextUrl.length) {
+      if (typeof data.message === 'string' && data.message.length) {
+        $.cxDialog({
+          title: '提示',
+          info: data.message,
+          ok: function() {
+            if (data.nextUrl === 'reload') {
+              location.reload();
+            } else {
+              location.href = data.nextUrl;
+            };
+          }
+        });
+      } else {
+        if (data.nextUrl === 'reload') {
+          location.reload();
+        } else {
+          location.href = data.nextUrl;
+        };
+      };
+
+    } else {
+      $.cxDialog({
+        title: '提示',
+        info: data.message
+      });
+    };
+  };
+
+  formAjax.submit = function(form, options, errorCallback) {
+    var self = this;
     var defaults = {
       url: form.attr('action'), 
       type: form.attr('method'),
@@ -424,11 +99,11 @@
       };
     };
 
-    options = $.extend({}, defaults, options);
-
     if (typeof errorCallback === 'function') {
       options.error = errorCallback;
     };
+
+    options = $.extend({}, defaults, options);
 
     if (Array.isArray(options.addData) && options.addData.length) {
       options.data = options.data.concat(options.addData);
@@ -453,12 +128,10 @@
       if (data.state !== 'success') {
         if (typeof options.error === 'function') {
           options.error(data);
-        } else {
-          $.cxDialog({
-            title: '提示',
-            info: data.message
-          });
+          return;
         };
+
+        self.finish(data);
         return;
       };
 
@@ -467,33 +140,7 @@
         return;
       };
 
-      if (typeof data.nextUrl === 'string' && data.nextUrl.length) {
-        if (typeof data.message === 'string' && data.message.length) {
-          $.cxDialog({
-            title: '提示',
-            info: data.message,
-            ok: function() {
-              if (data.nextUrl === 'reload') {
-                location.reload();
-              } else {
-                location.href = data.nextUrl;
-              };
-            }
-          });
-        } else {
-          if (data.nextUrl === 'reload') {
-            location.reload();
-          } else {
-            location.href = data.nextUrl;
-          };
-        };
-
-      } else {
-        $.cxDialog({
-          title: '提示',
-          info: data.message
-        });
-      };
+      self.finish(data);
 
     }).fail(function(jqXHR, textStatus, errorThrown){
       form.find('button[type="submit"]').prop('disabled', false);
@@ -504,6 +151,8 @@
       });
     });
   };
+
+  window.CallFormAjax = formAjax.bind(formAjax);
 })();
 
 
@@ -709,6 +358,8 @@
       height: options.size
     });
 
+    var bd = $('<div></div>', {'class': 'bd'}).html(pic);
+
     var qrcode = new QRCode(pic[0], {
       text: info,
       width: options.size,
@@ -718,16 +369,16 @@
       correctLevel : QRCode.CorrectLevel[options.quality]
     });
 
-    box.html(pic);
+    box.html(bd);
 
     if (typeof options.baseclass === 'string' && options.baseclass.length) {
       wrap.attr('class', options.baseclass);
     };
     if (typeof options.before === 'string' && options.before.length) {
-      box.prepend('<div class="before">' + options.before + '</div>');
+      box.prepend('<div class="hd">' + options.before + '</div>');
     };
     if (typeof options.after === 'string' && options.after.length) {
-      box.append('<div class="after">' + options.after + '</div>');
+      box.append('<div class="ft">' + options.after + '</div>');
     };
 
     wrap.appendTo('body').show();
--- js/controller/reday.js
+++ js/controller/reday.js
@@ -1,58 +1,56 @@
-(function(){
-  // FastClick Only iOS
-  // 在 Android 低端机下，效果不理想
-  if ('addEventListener' in document && /(iphone|ipad|ipod|ios)/i.test(navigator.userAgent.toLowerCase())) {
-    document.addEventListener('DOMContentLoaded', function() {
-      FastClick.attach(document.body);
-    }, false);
+// FastClick Only iOS
+// 在 Android 低端机下，效果不理想，不使用
+if ('addEventListener' in document && /(iphone|ipad|ipod|ios)/i.test(navigator.userAgent.toLowerCase())) {
+  document.addEventListener('DOMContentLoaded', function() {
+    FastClick.attach(document.body);
+  }, false);
+};
+
+// cxDialog 默认设置
+$.cxDialog.defaults.baseClass = 'ios';
+$.cxDialog.defaults.background = 'rgba(0,0,0,0.4)';
+$.cxDialog.defaults.ok = function(){};
+
+// 全局操作
+$('body').on('click', 'a', function(){
+  var _rel = this.rel;
+  var _rev = this.rev;
+  var _opt;
+
+  try {
+    _opt = JSON.parse(this.dataset.option);
+  } catch (e) {
+    _opt = {};
   };
 
-  // cxDialog 默认设置
-  $.cxDialog.defaults.baseClass = 'ios';
-  $.cxDialog.defaults.background = 'rgba(0,0,0,0.4)';
-  $.cxDialog.defaults.ok = function(){};
+  // 显示二维码
+  if (_rel === 'call_qrcode') {
+    window.CallQrcode.show(_rev, _opt);
+    return false;
 
-  // 全局操作
-  $('body').on('click', 'a', function(){
-    var _rel = this.rel;
-    var _rev = this.rev;
-    var _opt;
+  // 发送短信验证码
+  } else if (_rel === 'call_sms') {
+    window.CallSms.send(this);
+    return false;
 
-    try {
-      _opt = JSON.parse(this.dataset.option);
-    } catch (e) {
-      _opt = {};
-    };
+  // 显示微信分享提示
+  } else if (_rel === 'call_wechat_share') {
+    window.CallWechatTip.show(_rev);
+    return false;
+  };
+});
 
-    // 显示二维码
-    if (_rel === 'call_qrcode') {
-      window.CallQrcode.show(_rev, _opt);
-      return false;
-
-    // 发送短信验证码
-    } else if (_rel === 'call_sms') {
-      window.CallSms.send(this);
-      return false;
-
-    // 显示微信分享提示
-    } else if (_rel === 'call_wechat_share') {
-      window.CallWechatTip.show(_rev);
-      return false;
-    };
-  });
-
-  // 顶部操作
-  $('#header').on('click', 'dt', function() {
-    var _dl = $(this).closest('dl');
-    if (_dl.hasClass('menu')) {
-      _dl.toggleClass('open');
-    } else {
-      setTimeout(function() {
-        _dl.removeClass('open');
-      }, 200);
-    };
-  });
-})();
+// 顶部操作
+$('#header').on('click', 'dt', function() {
+  var _dl = $(this).closest('dl');
+  if (_dl.hasClass('menu')) {
+    _dl.toggleClass('open');
+  } else {
+    setTimeout(function() {
+      _dl.removeClass('open');
+    }, 200);
+  };
+});
 
 
 // 解决 iOS 输入框获取焦点时 fixed 错位
@@ -104,4 +102,24 @@
   });
 
   box.prepend(pic).append('<p>微信扫一扫<br>获得更多内容</p>').appendTo('body');
+})();
+
+
+// 底部导航自适应兼容
+(function() {
+  var footerNav = document.getElementById('footer_nav');
+
+  if (!footerNav || /(iphone|ipad|ipod|ios)/i.test(navigator.userAgent.toLowerCase())) {
+    return;
+  };
+
+  // 如果支持 flex 则不需要 fix
+  if (!!((window.CSS && window.CSS.supports))) {
+    if (CSS.supports('display', 'flex')) {
+      return;
+    };
+  };
+
+  footerNav.classList.add('flex');
+  footerNav.classList.add('flex_' + footerNav.querySelectorAll('a').length);
 })();
